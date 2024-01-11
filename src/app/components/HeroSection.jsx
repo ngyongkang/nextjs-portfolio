dec0804ff57c1d777ba0b8794ece990d353a75b1
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-11 08:42:53 +0800

    Finished hero-sectioncomponent

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,61 @@
+"use client"
+import React from 'react'
+import Image from 'next/image'
+import { TypeAnimation } from 'react-type-animation'
+
+function HeroSection() {
+  return (
+    <section>
+        <div className='grid grid-cols-1 sm:grid-cols-12'>
+            <div className="col-span-7 place-self-center text-center sm:text-left">
+                <h1 className='text-white mb-4  text-4xl lg:text-6xl font-extrabold'>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>
+                        Hello, I am{" "}
+                    </span>
+                    <br/>
+                    <TypeAnimation
+                    sequence={[
+                        // Same substring at the start will only be typed out once, initially
+                        'Yong Kang',
+                        1000, // wait 1s before replacing "Mice" with "Hamsters"
+                        'Full-stack Developer',
+                        1000,
+                        'Devs Op Developer',
+                        1000,
+                        'UI/UX Beginner',
+                        1000
+                    ]}
+                    wrapper="span"
+                    speed={50}
+                    repeat={Infinity}
+                    />
+                </h1>
+                <p className='text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl'>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <div>
+                    <button className='px-6 py-3 rounded-full w-full sm:w-fit mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white'>Hire Me</button>
+                    <button className='px-1 py-1 rounded-full w-full sm:w-fit bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'>
+                        <span className='block bg-[#121212] hover:bg-slate-500 rounded-full px-5 py-2'>
+                            Download CV
+                        </span>
+                    </button>
+                </div>
+            </div>
+            <div className="col-span-5 place-self-center mt-4 lg:mt-0">
+                <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+                    <Image
+                        src="/images/hero-image3.png"
+                        alt="hero image"
+                        className='absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2'
+                        width={300}
+                        height={300}
+                    />
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default HeroSection
