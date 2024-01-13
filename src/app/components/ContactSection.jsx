d163c80bfea07cf8e4a1f26808b67615aea2c777
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-13 23:13:02 +0800

    Finished contact section component.

--- src/app/components/ContactSection.jsx
+++ src/app/components/ContactSection.jsx
@@ -27,14 +27,15 @@ const inputFields = [
 
 function ContactSection() {
   return (
-    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4'>
-        <div>
+    <section className='grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative'>
+        <div className='bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2'></div>
+        <div className='z-10'>
             <h5 className='text-xl font-bold text-white my-2'>
                 Let&apos;s Connect
             </h5>
             <p className='text-[#ADB7BE] mb-4 max-w-md'>
                 I&apos;m currently looking for new opportunities, my inbox is always open.
-                Whether you ahve a question or just want to say hi, I&apos;ll try my best
+                Whether you have a question or just want to say hi, I&apos;ll try my best
                 to get back to you!
             </p>
             <div className='socials flex flex-row gap-2'>
@@ -46,7 +47,7 @@ function ContactSection() {
                 </Link>
             </div>
         </div>
-        <div>
+        <div className='z-10'>
             <form className='flex flex-col'>
                 {
                     inputFields.map(function(inputObj, index) {
