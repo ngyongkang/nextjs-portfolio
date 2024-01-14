3e62b972813982ff61e461bcf746ad4641a626ed
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-14 10:16:58 +0800

    fixed z-axis on footer to be in front of mesh.

--- src/app/components/FooterSection.jsx
+++ src/app/components/FooterSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function FooterSection() {
   return (
-    <footer className='footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
+    <footer className='z-10 footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white'>
         <div className='container p-12 flex justify-between'>
             <span>
                 LOGO
