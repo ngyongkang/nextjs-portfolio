fa4f000e3e5c5b3c402ccb8d64059990c556ef83
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-14 14:53:37 +0800

    changed website title and added animations for project section.

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,7 +1,8 @@
-'use client'
-import React, {useState} from 'react'
-import ProjectCard from './ProjectCard'
+'use client';
+import React, { useState, useRef } from 'react';
+import ProjectCard from './ProjectCard';
 import ProjectTag from './ProjectTag';
+import { motion, useInView } from 'framer-motion';
 
 const projects = [
     {
@@ -65,6 +66,8 @@ const tags = ["All","Web","Mobile"];
 function ProjectSection() {
 
     const [tag, setTag] = useState("All");
+    const ref = useRef(null);
+    const isInView = useInView(ref, {once: true});
 
     function selectButton(newTag) {
         setTag(newTag);
@@ -72,41 +75,54 @@ function ProjectSection() {
 
     const filterProjects = projects.filter((project) => project.tag.includes(tag));
 
+    const cardVariants = {
+        initial: {y: 50, opacity: 0},
+        animate: {y: 0, opacity: 1},
+    }
+
     return (
-        <>
-        <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
-            My Projects
-        </h2>
-        <div className='flex flex-row justify-center items-center gap-2 py-6'>
-            {
-                tags.map(function(tagName, index){
-                    return (
-                        <ProjectTag 
-                        key={index} 
-                        name={tagName} 
-                        onClick={() => selectButton(tagName)} 
-                        isSelected={tagName === tag}/>
-                    )
-                })
-            }
-        </div>
-        <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-            {
-                filterProjects.map(function(project){
-                    return (
-                        <ProjectCard 
-                        key={project.id} 
-                        title={project.title} 
-                        description={project.description}
-                        imgUrl={project.image} 
-                        previewUrl={project.previewUrl}
-                        gitUrl={project.gitUrl}
-                        />
-                    )
-                })
-            }
-        </div>
-        </>
+        <section>
+            <h2 className='text-center text-4xl font-bold text-white mt-4 mb-8 md:mb-12'>
+                My Projects
+            </h2>
+            <div className='flex flex-row justify-center items-center gap-2 py-6'>
+                {
+                    tags.map(function(tagName, index){
+                        return (
+                            <ProjectTag 
+                            key={index} 
+                            name={tagName} 
+                            onClick={() => selectButton(tagName)} 
+                            isSelected={tagName === tag}/>
+                        )
+                    })
+                }
+            </div>
+            <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
+                {
+                    filterProjects.map(function(project, index){
+                        return (
+                            <motion.li 
+                            key={index}
+                            variants={cardVariants}
+                            initial="initial"
+                            animate={isInView ? "animate":"initial"}
+                            transition={{duration: 0.3, delay: index * 0.4}}
+                            >
+                                <ProjectCard 
+                                key={project.id}
+                                title={project.title} 
+                                description={project.description}
+                                imgUrl={project.image} 
+                                previewUrl={project.previewUrl}
+                                gitUrl={project.gitUrl}
+                                />
+                            </motion.li>
+                        )
+                    })
+                }
+            </ul>
+        </section>
         
     )
 }
