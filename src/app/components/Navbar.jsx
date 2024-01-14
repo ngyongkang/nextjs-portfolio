b388464cd34bbdaa823b7991a7c83c8f920c6f2c
ngyongkang <81545663+ngyongkang@users.noreply.github.com>
2024-01-14 10:14:45 +0800

    Fixed z axis problem on navbar.

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -24,7 +24,7 @@ function NavBar() {
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     return (
-        <nav className='mx-auto border border-[#33353F] fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100'>
+        <nav className='mx-auto border border-[#33353F] fixed top-0 left-0 right-0 z-20 bg-[#121212] bg-opacity-100'>
             <div className="container lg:py-4 flex flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link href={"/"} className='text-2xl md:text-5xl text-white font-semibold'>LOGO</Link>
                 
